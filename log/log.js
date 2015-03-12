3a83c9ca8a3ae2e71739df385c35e4790d9a0071
Alan Litteneker <alan.litteneker@gmail.com>
2015-03-12 01:01:21 -0700

    Part of part 3 working, but not all

--- log/log.js
+++ log/log.js
@@ -6,23 +6,18 @@ Rule._seed_uid = 0;
 Rule.prototype.getFreshName = function() {
   return '_' + ++Rule._seed_uid;
 }
-Rule.prototype.makeCopyWithFreshVarNames = function() {
+Rule.prototype.makeFresh = function(item, idMap) {
   var me = this;
-  var ids = {};
-  return new Rule(
-    new Clause(this.head.name, this.head.args.map(function(arg) {
-      if( !ids[arg] )
-        ids[arg] = me.getFreshName();
-      return new Var(ids[arg]);
-    })),
-    this.body.map(function(val) {
-      return new Clause(val.name, val.args.map(function(arg) {
-        if( !ids[arg] )
-          ids[arg] = me.getFreshName();
-        return new Var(ids[arg]);
-      }));
-    })
-  );
+  if( item instanceof Var ) {
+    if( !idMap[item.name] )
+      idMap[item.name] = me.getFreshName();
+    return new Var(idMap[item.name]);
+  }
+  return new Clause( item.name, item.args.map( function(arg) { return me.makeFresh(arg, idMap); }));
+}
+Rule.prototype.makeCopyWithFreshVarNames = function() {
+  var me = this, ids = {};
+  return new Rule(this.makeFresh(me.head, ids), this.body.map(function(val) { return me.makeFresh(val, ids); }));
 };
 
 Clause.prototype.rewrite = function(subst) {
@@ -31,8 +26,8 @@ Clause.prototype.rewrite = function(subst) {
 
 Var.prototype.rewrite = function(subst) {
   if( subst.lookup(this.name) )
-    return subst.lookup(this.name);
-  return this;
+    return subst.lookup(this.name).rewrite(subst);
+  return new Var(this.name);
 };
 
 // -----------------------------------------------------------------------------
@@ -50,14 +45,14 @@ Subst.prototype.unify = function(term1, term2) {
     this.bind.apply(this, term1 instanceof Var ? [term1.name, term2] : [term2.name, term1]);
     return this;
   }
-  if( term1 instanceof Clause && term2 instanceof Clause ) {
-    if( term1.name === term2.name && term1.args.length === term2.args.length ) {
-      for( var i = 0; i < term1.args.length; ++i ) {
-        if( !this.unify(term1.args[i], term2.args[i]) )
-          return undefined;
-      }
-      return this;
+  if( term1 instanceof Clause && term2 instanceof Clause
+    && term1.name === term2.name && term1.args.length === term2.args.length )
+  {
+    for( var i = 0; i < term1.args.length; ++i ) {
+      if( !this.unify(term1.args[i], term2.args[i]) )
+        return undefined;
     }
+    return this;
   }
   throw new Error("Unable to unify");
 };
@@ -66,6 +61,90 @@ Subst.prototype.unify = function(term1, term2) {
 // Part III: Program.prototype.solve()
 // -----------------------------------------------------------------------------
 
-Program.prototype.solve = function() {
-  throw new TODO("Program.prototype.solve not implemented");
+function tryUnify(subst, term1, term2) {
+  var match;
+  try {
+    match = subst.clone().unify( term1, term2 );
+  } catch(e) {
+    match = undefined;
+  }
+  return match;
+}
+
+// walk through the rules to try to find a solution to the given query, then check the nextQuery
+function RuleWalker(program, query, nextQuery) {
+  this.idx = 0;
+  this.query = query;
+  this.nextQuery = nextQuery;
+  this.set = program.rules;
+  this.program = program;
+  this.bodyWalker = false;
+}
+RuleWalker.prototype.setQuery = function(queryClause) {
+  this.query = queryClause;
+  this.walker.reset();
 };
+RuleWalker.prototype.nextSolution = function(subst) {
+  while( this.idx < this.set.length ) {
+    var rule = this.program.rules[this.idx].makeCopyWithFreshVarNames();
+    var disableIncrement = false;
+
+    // Unify with rule head
+    var match = tryUnify( subst, this.query, rule.head );
+
+    // Unify with rule body, if it exists
+    if( match && rule.body.length > 0 ) {
+      if( !this.bodyWalker || this.bodyWalker.set !== rule.body )
+        this.bodyWalker = new QueryChecker(this.program, rule.body, 0);
+      if( match = this.bodyWalker.next(match) )
+        disableIncrement = true;
+    }
+    else
+      this.bodyWalker = false;
+
+    if( match ) {
+      // If we have a match from our current item, try to unify this solution with the remaining query terms
+      match = this.nextQuery.next(match);
+
+      // need to be sure to increment before returning if we have no child queries
+      if( !disableIncrement && ( !this.nextQuery.query || !match ) )
+        this.increment();
+
+      // If we have a match, return this next solution
+      if( match )
+        return match;
+    }
+    else
+      this.increment();
+  }
+};
+RuleWalker.prototype.increment = function() {
+  this.nextQuery && this.nextQuery.reset();
+  this.idx++;
+};
+RuleWalker.prototype.reset = function() {
+  this.idx = 0;
+};
+
+// recursively check each query, to make sure they are all accurate together
+function QueryChecker(program, clauses, queryIdx) {
+  this.query = clauses[queryIdx];
+  this.nextQuery = this.query ? new QueryChecker(program, clauses, queryIdx + 1) : null;
+  this.walker = this.query ? new RuleWalker(program, this.query, this.nextQuery) : null;
+}
+QueryChecker.prototype.next = function(subst) {
+  if( !this.query )
+    return subst;
+  return this.walker.nextSolution(subst || new Subst());
+};
+QueryChecker.prototype.reset = function() {
+  this.walker && this.walker.reset();
+};
+Program.prototype.solve = function() {
+  return new QueryChecker(this, this.query, 0);
+};
+
+
+
+
+//----------
